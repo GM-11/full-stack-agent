d46bc11bf7dd21313d2dcd1fbc7dd3068304da1a
GM-11 <mathuyrgopal@gmail.com>
2025-01-29 13:54:27 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="full-stack-agent",
+    version="0.1.0",
+    author="Gopal Mathur",
+    author_email="mathurgopal1001@gmail.com",
+    description="A CLI tool for initializing full-stack projects with best practices",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GM-11/full-stack-agent",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "langchain-groq",
+        "langchain",
+        "langchain-community",
+        "inquirer",
+        "python-dotenv",
+        "tavily-python",
+    ],
+    entry_points={
+        "console_scripts": [
+            "full-stack-agent=main:main",
+        ],
+    },
+)
